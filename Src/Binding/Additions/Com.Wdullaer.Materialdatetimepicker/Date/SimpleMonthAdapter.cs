2ab986bfa8cc4af35b96695e24617fd930058847
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-08-26 19:30:07 +0200

    Update to metadata to get project compiling

---
+++ Src/Binding/Additions/Com.Wdullaer.Materialdatetimepicker/Date/SimpleMonthAdapter.cs
@@ -0,0 +1,12 @@
+﻿using Android.Support.V7.Widget;
+
+namespace Com.Wdullaer.Materialdatetimepicker.Date
+{
+    public partial class SimpleMonthAdapter
+    {
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            (holder as MonthViewHolder)?.Bind(position, MController, SelectedDay);
+        }
+    }
+}
