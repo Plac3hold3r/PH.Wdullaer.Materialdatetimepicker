09d2d1a785d3296f29e9492b33ef4a461f59e5ba
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-08-26 16:19:51 +0200

    Fixed binding project to compile

---
+++ Src/Binding/Additions/Com.Wdullaer.Materialdatetimepicker/Time/Timepoint.cs
@@ -0,0 +1,10 @@
+﻿namespace Com.Wdullaer.Materialdatetimepicker.Time
+{
+    public partial class Timepoint
+    {
+        int Java.Lang.IComparable.CompareTo(Java.Lang.Object o)
+        {
+            return CompareTo((Timepoint)o);
+        }
+    }
+}
