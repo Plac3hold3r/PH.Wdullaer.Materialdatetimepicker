124c9cc0f8d3b05abe8d97741f600827f9d75335
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-09-02 14:39:38 +0200

    Add date and time fragment content

--- test/TestApp/Views/DatePickerFragment.cs
+++ test/TestApp/Views/DatePickerFragment.cs
@@ -1,33 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
+﻿using Android.App;
+using Android.Graphics;
 using Android.OS;
-using Android.Runtime;
-using Android.Util;
 using Android.Views;
 using Android.Widget;
+using Java.Util;
+using Materialdatetimepicker = Com.Wdullaer.Materialdatetimepicker.Date;
 
 namespace TestApp.Views
 {
-    public class DatePickerFragment : Fragment
+    public class DatePickerFragment : Fragment, Materialdatetimepicker.DatePickerDialog.IOnDateSetListener
     {
-        public override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-
-            // Create your fragment here
-        }
+        private TextView dateTextView;
+        private CheckBox modeDarkDate;
+        private CheckBox modeCustomAccentDate;
+        private CheckBox vibrateDate;
+        private CheckBox dismissDate;
+        private CheckBox titleDate;
+        private CheckBox showYearFirst;
+        private CheckBox showVersion2;
+        private CheckBox limitSelectableDays;
+        private CheckBox highlightDays;
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            // Use this to return your custom view for this Fragment
-            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+            View view = inflater.Inflate(Resource.Layout.datepicker_layout, container, false);
 
-            return base.OnCreateView(inflater, container, savedInstanceState);
+            // Find our View instances
+            dateTextView = view.FindViewById<TextView>(Resource.Id.date_textview);
+            Button dateButton = view.FindViewById<Button>(Resource.Id.date_button);
+            modeDarkDate = view.FindViewById<CheckBox>(Resource.Id.mode_dark_date);
+            modeCustomAccentDate = view.FindViewById<CheckBox>(Resource.Id.mode_custom_accent_date);
+            vibrateDate = view.FindViewById<CheckBox>(Resource.Id.vibrate_date);
+            dismissDate = view.FindViewById<CheckBox>(Resource.Id.dismiss_date);
+            titleDate = view.FindViewById<CheckBox>(Resource.Id.title_date);
+            showYearFirst = view.FindViewById<CheckBox>(Resource.Id.show_year_first);
+            showVersion2 = view.FindViewById<CheckBox>(Resource.Id.show_version_2);
+            limitSelectableDays = view.FindViewById<CheckBox>(Resource.Id.limit_dates);
+            highlightDays = view.FindViewById<CheckBox>(Resource.Id.highlight_dates);
+
+            dateButton.Click += DateButton_Click;
+
+            return view;
+        }
+
+        private void DateButton_Click(object sender, System.EventArgs e)
+        {
+            Calendar now = Calendar.Instance;
+            var dpd = new Materialdatetimepicker.DatePickerDialog();
+            dpd.Initialize(
+                    this,
+                    now.Get(CalendarField.Year),
+                    now.Get(CalendarField.Month),
+                    now.Get(CalendarField.DayOfMonth)
+            );
+            dpd.SetThemeDark(modeDarkDate.Checked);
+            dpd.Vibrate(vibrateDate.Checked);
+            dpd.DismissOnPause(dismissDate.Checked);
+            dpd.ShowYearPickerFirst(showYearFirst.Checked);
+            dpd.SetVersion(showVersion2.Checked ? Materialdatetimepicker.DatePickerDialog.Version.Version2 : Materialdatetimepicker.DatePickerDialog.Version.Version1);
+            if (modeCustomAccentDate.Checked)
+            {
+                dpd.AccentColor = Color.ParseColor("#9C27B0");
+            }
+            if (titleDate.Checked)
+            {
+                dpd.SetTitle("DatePicker Title");
+            }
+            if (highlightDays.Checked)
+            {
+                Calendar date1 = Calendar.Instance;
+                Calendar date2 = Calendar.Instance;
+                date2.Add(CalendarField.WeekOfMonth, -1);
+                Calendar date3 = Calendar.Instance;
+                date3.Add(CalendarField.WeekOfMonth, 1);
+                Calendar[] days = { date1, date2, date3 };
+                dpd.SetHighlightedDays(days);
+            }
+            if (limitSelectableDays.Checked)
+            {
+                Calendar[] days = new Calendar[13];
+                for (int i = -6; i < 7; i++)
+                {
+                    Calendar day = Calendar.Instance;
+                    day.Add(CalendarField.DayOfMonth, i * 2);
+                    days[i + 6] = day;
+                }
+                dpd.SetSelectableDays(days);
+            }
+            dpd.Show(FragmentManager, "Datepickerdialog");
+        }
+
+        public override void OnResume()
+        {
+            base.OnResume();
+            var dpd = (Materialdatetimepicker.DatePickerDialog)FragmentManager.FindFragmentByTag("Datepickerdialog");
+            if (dpd != null) dpd.SetOnDateSetListener(this);
+        }
+
+        public void OnDateSet(Materialdatetimepicker.DatePickerDialog p0, int p1, int p2, int p3)
+        {
+            string date = "You picked the following date: " + p3 + "/" + (++p2) + "/" + p1;
+            dateTextView.Text = date;
         }
     }
 }
--- test/TestApp/Views/TimePickerFragment.cs
+++ test/TestApp/Views/TimePickerFragment.cs
@@ -1,33 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 using Android.App;
-using Android.Content;
+using Android.Graphics;
 using Android.OS;
-using Android.Runtime;
-using Android.Util;
 using Android.Views;
 using Android.Widget;
+using Java.Util;
+using Materialdatetimepicker = Com.Wdullaer.Materialdatetimepicker.Time;
 
 namespace TestApp.Views
 {
-    public class TimePickerFragment : Fragment
+    public class TimePickerFragment : Fragment, Materialdatetimepicker.TimePickerDialog.IOnTimeSetListener
     {
-        public override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-
-            // Create your fragment here
-        }
+        private TextView timeTextView;
+        private CheckBox mode24Hours;
+        private CheckBox modeDarkTime;
+        private CheckBox modeCustomAccentTime;
+        private CheckBox vibrateTime;
+        private CheckBox dismissTime;
+        private CheckBox titleTime;
+        private CheckBox enableSeconds;
+        private CheckBox limitSelectableTimes;
+        private CheckBox showVersion2;
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            // Use this to return your custom view for this Fragment
-            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+            View view = inflater.Inflate(Resource.Layout.timepicker_layout, container, false);
 
-            return base.OnCreateView(inflater, container, savedInstanceState);
+            // Find our View instances
+            timeTextView = view.FindViewById<TextView>(Resource.Id.time_textview);
+            Button timeButton = view.FindViewById<Button>(Resource.Id.time_button);
+            mode24Hours = view.FindViewById<CheckBox>(Resource.Id.mode_24_hours);
+            modeDarkTime = view.FindViewById<CheckBox>(Resource.Id.mode_dark_time);
+            modeCustomAccentTime = view.FindViewById<CheckBox>(Resource.Id.mode_custom_accent_time);
+            vibrateTime = view.FindViewById<CheckBox>(Resource.Id.vibrate_time);
+            dismissTime = view.FindViewById<CheckBox>(Resource.Id.dismiss_time);
+            titleTime = view.FindViewById<CheckBox>(Resource.Id.title_time);
+            enableSeconds = view.FindViewById<CheckBox>(Resource.Id.enable_seconds);
+            limitSelectableTimes = view.FindViewById<CheckBox>(Resource.Id.limit_times);
+            showVersion2 = view.FindViewById<CheckBox>(Resource.Id.show_version_2);
+
+            timeButton.Click += TimeButton_Click;
+
+            return view;
+        }
+
+        private void TimeButton_Click(object sender, System.EventArgs e)
+        {
+            Calendar now = Calendar.Instance;
+            var tpd = new Materialdatetimepicker.TimePickerDialog();
+            tpd.Initialize(
+                    this,
+                    now.Get(CalendarField.HourOfDay),
+                    now.Get(CalendarField.Minute),
+                    now.Get(CalendarField.Second),
+                    mode24Hours.Checked
+            );
+            tpd.SetThemeDark(modeDarkTime.Checked);
+            tpd.Vibrate(vibrateTime.Checked);
+            tpd.DismissOnPause(dismissTime.Checked);
+            tpd.EnableSeconds(enableSeconds.Checked);
+            tpd.Version = showVersion2.Checked ? Materialdatetimepicker.TimePickerDialog.AppVersion.Version2 : Materialdatetimepicker.TimePickerDialog.AppVersion.Version1;
+            if (modeCustomAccentTime.Checked)
+            {
+                tpd.AccentColor = Color.ParseColor("#9C27B0");
+            }
+            if (titleTime.Checked)
+            {
+                tpd.Title = "TimePicker Title";
+            }
+            if (limitSelectableTimes.Checked)
+            {
+                tpd.SetTimeInterval(3, 5, 10);
+            }
+
+            tpd.Show(FragmentManager, "Timepickerdialog");
+        }
+
+        public override void OnResume()
+        {
+            base.OnResume();
+            var tpd = (Materialdatetimepicker.TimePickerDialog)FragmentManager.FindFragmentByTag("Timepickerdialog");
+            if (tpd != null) tpd.SetOnTimeSetListener(this);
+        }
+
+        public void OnTimeSet(Materialdatetimepicker.TimePickerDialog p0, int p1, int p2, int p3)
+        {
+            string hourString = p1 < 10 ? "0" + p1 : "" + p1;
+            string minuteString = p2 < 10 ? "0" + p2 : "" + p2;
+            string secondString = p3 < 10 ? "0" + p3 : "" + p3;
+            string time = "You picked the following time: " + hourString + "h" + minuteString + "m" + secondString + "s";
+            timeTextView.Text = time;
         }
     }
 }
